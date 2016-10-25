99c7c0d098d9ff0a87599e6effc8b8f4205d9bbc
Daniel Houtsma <danhoutsma@gmail.com>
2016-10-25 03:15:00 -0700

    Start basic implementation

--- lib/onebusaway.rb
+++ lib/onebusaway.rb
@@ -4,4 +4,25 @@ Dir[__dir__ + '/onebusaway/*.rb'].each do |file|
 end
 
 module OneBusAway
+  class << self
+    BASE_URL = 'http://api.onebusaway.org/api/where/'.freeze
+
+    attr_writer :api_key
+
+    def agency(id)
+      response = request("agency/#{id}")
+      Agency.new(response['data']['entry'])
+    end
+
+    private
+
+    def request(call, options = {})
+      raise 'API key not set!' unless @api_key
+
+      options[:key] = @api_key
+
+      response = HTTParty.get("#{BASE_URL}#{call}.json", query: options)
+      JSON.parse(response.body)
+    end
+  end
 end
--- lib/onebusaway/agency.rb
+++ lib/onebusaway/agency.rb
@@ -1,4 +1,27 @@
 module OneBusAway
   class Agency
+    attr_reader :disclaimer,
+                :email,
+                :fare_url,
+                :id,
+                :lang,
+                :name,
+                :phone,
+                :private_service,
+                :timezone,
+                :url
+
+    def initialize(attributes)
+      @disclaimer = attributes['disclaimer']
+      @email = attributes['email']
+      @fare_url = attributes['fareUrl']
+      @id = attributes['id']
+      @lang = attributes['lang']
+      @name = attributes['name']
+      @phone = attributes['phone']
+      @private_service = attributes['privateService']
+      @timezone = attributes['timezone']
+      @url = attributes['url']
+    end
   end
 end
